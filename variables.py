f9fc595696773f7a80b88c27ba49b0d042f87d83
Taylor Sather <fivetentaylor@gmail.com>
2014-09-27 20:43:19 -0700

    Progressing towards a shortes path solution

--- variables.py
+++ variables.py
@@ -10,16 +10,33 @@ stats = ['def_int','def_int_long','def_int_td','def_int_yds','fga','fgm','fumble
 data = pd.read_csv('db.csv')
 data['game_id'] = data['url'].factorize()[0]
 
-def build_graph(data):
+def build_graph(data, pred=lambda x: True):
 	G = nx.MultiDiGraph()
 	G.add_nodes_from(data.id.unique())
-	itty = data[['id','id_opp','year','week']].itertuples()
-	stat_vectors = data[stats].fillna(0).itertuples()
+	itty = data[data.apply(pred, axis=1)][['id','id_opp','year','week','host']].itertuples()
+	stat_vectors = data[data.apply(pred, axis=1)][stats].fillna(0).itertuples()
 	def edge(x,y):
-		return (x[1], x[2], {'year':x[3], 'week':x[4], 'stats':np.array(y)})
+		return (x[1], x[2], {'year':x[3], 'week':x[4], 'host':x[5], 'stats':np.array(y)})
 	G.add_edges_from([edge(*x) for x in it.izip(itty, stat_vectors)])
 	return G
 
 def game_attributes(home, visitor, year, week):
 	pass
+
+def prevN(week, N=3):
+	return [((week - x) % 16) + 1 for x in xrange(2,2+N)]
+
+def gameFilter(year, week):
+	def pred(rec):
+		last3 = prevN(week, N=3)
+		return (rec.year == year) and (rec.week in last3)
+	return pred
+	
+graph = build_graph(data, gameFilter(2010, 4))
+
+def N_shortest(graph, home, visitor, N=5):
+	return sorted(list(nx.all_simple_paths(graph, home, visitor, N)), cmp=lambda x,y: len(x) - len(y))
+
+
+
 
